6b1a571e27cdf088476853d4c1a7b8b9177b3bdf
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-09-09 13:43:17 +0200

    add checkWin function

--- script.js
+++ script.js
@@ -39,9 +39,11 @@ function gameBoard() {
     const placeToken = (row, column, player) => {
         if (board[row][column].getValue() === "-") {
             board[row][column].addToken(player);
+            return true;
         }
         else {
             console.log("Invalid position, try again")
+            return false;
         }
     }
 
@@ -51,3 +53,106 @@ function gameBoard() {
         placeToken
     };
 }
+
+function gameController(playerOneName = "Player1", 
+                        playerTwoName = "Player2") {
+    
+    const players = [
+        {
+            name: playerOneName,
+            token: "X"
+        },
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ];
+
+    const board = gameBoard();
+
+    let activePlayer = players[0]
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+    
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`It is ${activePlayer.name}'s turn.`)
+    }
+
+    const checkWin = (row, column, token) => {
+        let n = 3; // n = board size
+        const playBoard = board.getBoard();
+        //check horizontals
+        for (let i = 0; i < n; i++) {
+            if (playBoard[row][i].getValue() != token) break;
+            
+            if (i == n - 1) {
+                board.printBoard();
+                console.log(`${getActivePlayer().name} wins!`)
+                return true;
+            }
+        }
+        //check verticals
+        for (let i = 0; i < n; i++) {
+            if (playBoard[i][column].getValue() != token) break;
+            
+            if (i == n - 1) {
+                board.printBoard();
+                console.log(`${getActivePlayer().name} wins!`)
+                return true;
+            }
+        //check diagonals
+            if (row === column) {
+                for (let i = 0; i < n; i++) {
+                    if (playBoard[i][i].getValue() != token) break;
+
+                    if (i == n - 1) {
+                        board.printBoard();
+                        console.log(`${getActivePlayer().name} wins!`)
+                        return true;
+                    }
+                }
+            }
+
+            if (row + column === n - 1) {
+                for (let i = 0; i < n; i++) {
+                    if (playBoard[i][n - 1 - i].getValue() != token) break;
+
+                    if (i == n - 1) {
+                        board.printBoard();
+                        console.log(`${getActivePlayer().name} wins!`)
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    const playRound = (row, column) => {
+        
+        console.log(`Placing token at ${row} ${column}`)
+        let round = board.placeToken(row, column, getActivePlayer().token);
+
+
+        
+
+        if (round) {
+            if (checkWin(row, column, getActivePlayer().token)) return
+            switchPlayer();
+            printNewRound();
+        }
+    }
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    }
+}
+
+const game = gameController();
